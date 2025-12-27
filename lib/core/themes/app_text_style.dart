5e6ea4dd6d541f716c07a9c1f464dc6364fd3aaa
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-12-27 16:19:41 +0200

    refactor some code

--- lib/core/themes/app_text_style.dart
+++ lib/core/themes/app_text_style.dart
@@ -1,20 +1,21 @@
 import 'package:doc_doc/core/themes/app_colors.dart';
 import 'package:doc_doc/core/themes/app_font_weight.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTextStyle {
-  static const TextStyle font32BoldWithMainBlue = TextStyle(
-    fontSize: 32,
+  static TextStyle font32BoldWithMainBlue = TextStyle(
+    fontSize: 32.sp,
     fontWeight: AppFontWeight.bold,
     color: AppColors.mainBlue,
   );
-  static const TextStyle font10RegularWithGrey = TextStyle(
-    fontSize: 10,
+  static TextStyle font10RegularWithGrey = TextStyle(
+    fontSize: 10.sp,
     fontWeight: AppFontWeight.regular,
     color: AppColors.grey,
   );
-  static const TextStyle font16SemiBoldWithWhite = TextStyle(
-    fontSize: 16,
+  static TextStyle font16SemiBoldWithWhite = TextStyle(
+    fontSize: 16.sp,
     fontWeight: AppFontWeight.semiBold,
     color: AppColors.mainWhite,
   );
