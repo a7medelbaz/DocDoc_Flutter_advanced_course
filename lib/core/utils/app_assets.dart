3c785d13a203d6bada07db72e67b93ffac07d419
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-12-28 00:48:37 +0200

    adding new colors , text styles , assets and editing the onboarding screen

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -5,4 +5,7 @@ class AppAssets {
       'assets/svgs/doc_low_opacity_logo.svg';
   static const doctorImage = 'assets/images/doctor_image.png';
   static const splashScreen = 'assets/images/splash_screen.png';
+  static const googleIcon = 'assets/svgs/google_icon.svg';
+  static const facebookIcon = 'assets/svgs/facebook_icon.svg';
+  static const appleIcon = 'assets/svgs/apple_icon.svg';
 }
