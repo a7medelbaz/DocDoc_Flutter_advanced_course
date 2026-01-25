a05ae733620a171a1394f2ffa3588553b624ed44
Ahmed_Elbaz <aelbaz0189@gmail.com>
2026-01-25 17:26:39 +0200

    feat: Add ApiResult class for handling API responses with success and failure states

---
+++ lib/core/networking/api_results.dart
@@ -0,0 +1,11 @@
+import 'package:doc_doc/core/networking/api_error_handler.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'api_results.freezed.dart';
+
+@freezed
+class ApiResult<T> with _$ApiResult<T> {
+  const factory ApiResult.success(T data) = Success<T>;
+  const factory ApiResult.failure(ErrorHandler errorHandler) =
+      Failure<T>;
+}
