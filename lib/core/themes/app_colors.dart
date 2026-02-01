2d40d40c4af2eb1a9beb8f0a4482df292ff9b8bb
Ahmed_Elbaz <aelbaz0189@gmail.com>
2026-02-01 22:59:14 +0200

    refactor: - fix grey to gray - add some needed colors - add some needed font styles

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -4,8 +4,10 @@ class AppColors {
   static const Color mainBlue = Color(0xff247CFF);
   static const Color mainWhite = Color(0xffFFFFFF);
   static const Color whiteSmoke = Color(0xffF5F5F5);
-  static const Color grey = Color(0xff757575);
-  static const Color grey400 = Color(0xffA9B2B9);
+  static const Color gray = Color(0xff757575);
+  static const Color gray400 = Color(0xffA9B2B9);
+  static const Color moreLightGray = Color(0xFFF5F5F5);
   static const Color hintColor = Color(0xffC2C2C2);
   static const Color textFieldBorderColor = Color(0xffEDEDED);
+  static const Color darkBlue = Color(0xff242424);
 }
--- lib/core/themes/app_text_style.dart
+++ lib/core/themes/app_text_style.dart
@@ -14,30 +14,50 @@ class AppTextStyle {
     fontWeight: AppFontWeight.bold,
     color: AppColors.mainBlue,
   );
-  static TextStyle font10RegularWithGrey = TextStyle(
+  static TextStyle font10RegularWithGray = TextStyle(
     fontSize: 10.sp,
     fontWeight: AppFontWeight.regular,
-    color: AppColors.grey,
+    color: AppColors.gray,
   );
-  static TextStyle font12RegularWithGrey400 = TextStyle(
+  static TextStyle font12RegularWithGray400 = TextStyle(
     fontSize: 12.sp,
     fontWeight: AppFontWeight.regular,
-    color: AppColors.grey400,
+    color: AppColors.gray400,
   );
   static TextStyle font12RegularWithMainBlue = TextStyle(
     fontSize: 12.sp,
     fontWeight: AppFontWeight.regular,
     color: AppColors.mainBlue,
   );
-  static TextStyle font14RegularWithGrey = TextStyle(
+  static TextStyle font13RegularDarkBlue = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: AppFontWeight.regular,
+    color: AppColors.darkBlue,
+  );
+  static TextStyle font14RegularWithGray = TextStyle(
     fontSize: 14.sp,
     fontWeight: AppFontWeight.regular,
-    color: AppColors.grey,
+    color: AppColors.gray,
+  );
+  static TextStyle font14MediumDarkBlue = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: AppFontWeight.medium,
+    color: AppColors.darkBlue,
   );
   static TextStyle font14MediumWithHintColor = TextStyle(
     fontSize: 14.sp,
     fontWeight: AppFontWeight.medium,
     color: AppColors.hintColor,
+  );
+  static TextStyle font14SemiBoldBlue = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: AppFontWeight.semiBold,
+    color: AppColors.mainBlue,
+  );
+  static TextStyle font15MediumDarkBlue = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: AppFontWeight.medium,
+    color: AppColors.darkBlue,
   );
   static TextStyle font16SemiBoldWithWhite = TextStyle(
     fontSize: 16.sp,
