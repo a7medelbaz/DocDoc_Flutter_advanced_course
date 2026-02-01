b5ae95eec94eca3f3ed7c91a92f213a6126924ae
Ahmed_Elbaz <aelbaz0189@gmail.com>
2026-02-01 23:01:25 +0200

    feat: integrate login functionality with form validation and UI enhancements

--- lib/features/auth/logic/cubit/login_cubit.dart
+++ lib/features/auth/logic/cubit/login_cubit.dart
@@ -2,16 +2,33 @@ import 'package:doc_doc/core/networking/api_results.dart';
 import 'package:doc_doc/features/auth/data/models/login_request_body.dart';
 import 'package:doc_doc/features/auth/data/repos/login_repo.dart';
 import 'package:doc_doc/features/auth/logic/cubit/login_state.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   final LoginRepo _loginRepo;
   LoginCubit(this._loginRepo) : super(const LoginState.initial());
-  emitLoginState(LoginRequestBody loginRequestBody) async {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Future<void> close() {
+    emailController.dispose();
+    passwordController.dispose();
+    return super.close();
+  }
+
+  /// Login Method
+  emitLoginStates() async {
+    LoginRequestBody loginRequestBody = LoginRequestBody(
+      email: emailController.text,
+      password: passwordController.text,
+    );
     emit(const LoginState.loading());
     final response = await _loginRepo.login(loginRequestBody);
 
-    response.when( 
+    response.when(
       success: (data) {
         emit(LoginState.success(data));
       },
