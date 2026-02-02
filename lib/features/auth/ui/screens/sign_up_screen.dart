a9881bb4a17bc506776f2a5d03fd1e6857995354
Ahmed_Elbaz <aelbaz0189@gmail.com>
2026-02-02 19:29:27 +0200

    feat: - build the sign up screen

--- lib/features/auth/ui/screens/sign_up_screen.dart
+++ lib/features/auth/ui/screens/sign_up_screen.dart
@@ -1,12 +1,63 @@
+import 'package:doc_doc/core/helper/extensions.dart';
+import 'package:doc_doc/core/helper/spacing.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../core/Widgets/app_elevated_button.dart';
+import '../../../../core/routers/routes.dart';
+import '../widgets/policy_and_sign_up.dart';
+import '../widgets/registration_intro_text.dart';
+import '../widgets/signup_registration_field.dart';
+import '../widgets/social_login_buttons.dart';
+import '../widgets/social_login_divider.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Sign Up screen')),
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+              right: 31.0.w,
+              left: 31.0.w,
+              top: 50.h,
+              bottom: 24.h,
+            ),
+            child: Column(
+              children: [
+                RegistrationIntroText(
+                  introTitle: 'Create Account',
+                  introMassage:
+                      "Sign up now and start exploring all that our app has to offer. We're excited to welcome you to our community!",
+                ),
+                verticalSpace(17),
+                SignupRegistrationField(),
+                verticalSpace(32),
+                AppElevatedButton(
+                  onPressed: () {
+                    // ToDo Sign Action
+                  },
+                  text: 'Create Account',
+                ),
+                verticalSpace(32),
+                SocialLoginDivider(),
+                verticalSpace(32),
+                SocialLoginButtons(),
+                verticalSpace(32),
+                PolicyAndSignUp(
+                  registrationQuestionText:
+                      'You don\'t have an account',
+                  registrationOptionText: 'Login',
+                  registrationOptionNavigation: () =>
+                      context.pushNamed(Routes.loginScreen),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
