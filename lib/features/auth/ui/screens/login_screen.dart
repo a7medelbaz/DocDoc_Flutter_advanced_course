99178193ae48d623ee14193dd28b68d2c519c312
Ahmed_Elbaz <aelbaz0189@gmail.com>
2026-02-02 19:28:35 +0200

    refactor: - fix the padding of the screen - fix and add the RegistrationIntroText - fix the PolicyAndSignUp

--- lib/features/auth/ui/screens/login_screen.dart
+++ lib/features/auth/ui/screens/login_screen.dart
@@ -1,13 +1,14 @@
+import 'package:doc_doc/core/helper/extensions.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import '../../../../core/Widgets/app_elevated_button.dart';
+import '../../../../core/routers/routes.dart';
 import '../../logic/cubit/login_cubit.dart';
 import '../widgets/registration_intro_text.dart';
 import '../widgets/login_block_listener.dart';
 import '../widgets/policy_and_sign_up.dart';
-import '../widgets/registration_field.dart';
+import '../widgets/login_registration_field.dart';
 import '../widgets/social_login_buttons.dart';
 import '../widgets/social_login_divider.dart';
 
@@ -21,16 +22,20 @@ class LogInScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.only(
-              right: 31.0,
-              left: 31.0,
-              top: 50,
-              bottom: 24,
+              right: 31.0.w,
+              left: 31.0.w,
+              top: 50.h,
+              bottom: 24.h,
             ),
             child: Column(
               children: [
-                RegistrationIntroText(),
+                RegistrationIntroText(
+                  introTitle: 'Welcome Back',
+                  introMassage:
+                      "We're excited to have you back, can't wait to see what you've been up to since you last \nlogged in.",
+                ),
                 SizedBox(height: 36.h),
-                RegistrationField(),
+                LoginRegistrationField(),
                 SizedBox(height: 32.h),
                 AppElevatedButton(
                   onPressed: () {
@@ -43,7 +48,13 @@ class LogInScreen extends StatelessWidget {
                 SizedBox(height: 32.h),
                 SocialLoginButtons(),
                 SizedBox(height: 24.h),
-                PolicyAndSignUp(),
+                PolicyAndSignUp(
+                  registrationQuestionText:
+                      'You don\'t have an account? ',
+                  registrationOptionText: 'Sign Up',
+                  registrationOptionNavigation: () =>
+                      context.pushNamed(Routes.signUpScreen),
+                ),
                 const LoginBlocListener(),
               ],
             ),
