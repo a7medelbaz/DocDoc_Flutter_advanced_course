2fe2ae94be42a24f8ebeed6080838837536e5572
Ahmed_Elbaz <aelbaz0189@gmail.com>
2026-02-17 02:26:38 +0200

    add some important text

--- lib/core/helper/constants.dart
+++ lib/core/helper/constants.dart
@@ -3,3 +3,8 @@ bool isLoggedInUser = false;
 class SharedPrefKeys {
   static const String userToken = 'userToken';
 }
+
+class ConstantsText {
+  static const String homeNearestDoctorText =
+      'Book and\nschedule with\nnearest doctor';
+}
