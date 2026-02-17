f581b194d9de01968f21a98304d4f60d147ed74d
Ahmed_Elbaz <aelbaz0189@gmail.com>
2026-02-17 02:27:55 +0200

    refactor : refactor the app assets file

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -1,14 +1,24 @@
 class AppAssets {
-  static const appLogo = 'assets/svgs/doc_logo.svg';
-  static const appName = 'assets/svgs/app_name.svg';
+  // Base paths
+  static const String _svgPath = 'assets/svgs';
+  static const String _imagePath = 'assets/images';
+
+  // SVGs
+  static const appLogo = '$_svgPath/doc_logo.svg';
+  static const appName = '$_svgPath/app_name.svg';
   static const appLogoWithLowOpacity =
-      'assets/svgs/doc_low_opacity_logo.svg';
-  static const doctorImage = 'assets/images/doctor_image.png';
-  static const splashScreen = 'assets/images/splash_screen.png';
-  static const googleIcon = 'assets/svgs/google_icon.svg';
-  static const facebookIcon = 'assets/svgs/facebook_icon.svg';
-  static const appleIcon = 'assets/svgs/apple_icon.svg';
-  static const alertIcon = 'assets/svgs/Alert.svg';
-  static const shapeIcon = 'assets/svgs/Shape.svg';
-  static const notificationIcon = 'assets/svgs/Notification.svg';
+      '$_svgPath/doc_low_opacity_logo.svg';
+  static const googleIcon = '$_svgPath/google_icon.svg';
+  static const facebookIcon = '$_svgPath/facebook_icon.svg';
+  static const appleIcon = '$_svgPath/apple_icon.svg';
+  static const alertIcon = '$_svgPath/Alert.svg';
+  static const shapeIcon = '$_svgPath/Shape.svg';
+  static const notificationIcon = '$_svgPath/Notification.svg';
+  // static const nurseImage = '$_svgPath/nurse_image.svg';
+
+  // PNG Images
+  static const doctorImage = '$_imagePath/doctor_image.png';
+  static const splashScreen = '$_imagePath/splash_screen.png';
+  static const nearestDoctorBackground = '$_imagePath/Background.png';
+  static const nurseImage = '$_imagePath/nurse_image.png';
 }
